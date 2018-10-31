8f359ecf32fff1c889a67a2df52afaf2be39f6e3
Filipe Borba <filipefborba@gmail.com>
2018-10-31 00:06:28 -0300

    almost done

--- Projeto3_2/search_term.py
+++ Projeto3_2/search_term.py
@@ -1,125 +1,105 @@
-# import cv2
+import cv2
 import os
-# import numpy as np
-# from random import shuffle
-# import pickle
-# from gluoncv import model_zoo, data, utils
-# from matplotlib import pyplot as plt
-# import argparse
+from matplotlib import pyplot as plt
+from keras.preprocessing import image
+from keras.applications.mobilenet import preprocess_input, decode_predictions, MobileNet
+import numpy as np
+import pickle
+import argparse
 
-# def compute_descriptors(image):
-#     img = cv2.imread(image,0)
-#     surf = cv2.xfeatures2d.SURF_create(hessianThreshold=400, extended=True, upright=True)#300-500
-#     kp, des = surf.detectAndCompute(img,None)
-#     return kp, des
-
-# def read_image_descriptors(path, folders, max_items=5):
-#     paths = []
-#     descriptors = []
-#     for folder in folders:
-#         directory = sorted(os.listdir(path + folder))
-#         shuffle(directory)
-#         images = [f for f in directory if not f.startswith('.')][:max_items]
-#         for img in images:
-#             imgout = path + folder + "/" + img
-#             kp, des = compute_descriptors(imgout)
-#             descriptors.append(des)
-#             paths.append(imgout)
-#     return paths, np.vstack(descriptors)
-
-# def create_vocabulary(desc, sz=300):
-#     kmeans = KMeans(n_clusters=sz, random_state=0).fit(desc)
-#     return (kmeans.cluster_centers_, kmeans)
-
-# def draw_histogram(image, vocab):
-#     kp, desc = compute_descriptors(image)
-#     centers, kmeans = vocab
-#     closest = kmeans.predict(desc)
-    
-#     hist = np.ones(centers.shape[0])
-#     for e in closest:
-#         hist[e]+=1
-#     return hist
-
-# def compare_histograms(hist, hist_vocab):
-#     return chisquare(hist, f_exp = hist_vocab)[0]
-
-# def create_vocab_hists(paths, vocab):
-#     vocab_hists = []
-#     for e in paths:
-#         h = draw_histogram(e, vocab)
-#         vocab_hists.append((e, h))
-#     return vocab_hists
-
-# def find_similar_bvw(image, vocab, vocab_hists, show=False):
-#     hist = draw_histogram(image, vocab)
-    
-#     distances = []
-#     for vocab_h in vocab_hists:
-#         chi_squared = compare_histograms(hist, vocab_h[1])
-#         distances.append((vocab_h[0], chi_squared))
-        
-#     sorted_distances = sorted(distances, key=lambda x: x[1], reverse=False)[:3]
-#     img1 = cv2.imread(image,0)
-#     print("Search Image:", image)
-#     if (show == True):
-#         plt.imshow(img1, cmap="gray")
-#         plt.title("Search Image")
-#         plt.show()
-#     for match in sorted_distances:
-#         print("Found Image:", match[0])
-#         if (show == True):
-#             print("Chi_Squared:", match[1])
-#             img2 = cv2.imread(match[0],0)
-#             plt.imshow(img2, cmap="gray")
-#             plt.title("Found Image")
-#             plt.show()
-#         print("--------------")
-
-# ################################################
-
-# #Treinar
-# def train(test_path, folders, max_items=15):
-#     if not os.path.exists('./helper/'):
-#         os.makedirs('./helper/')
-#     print("Begin training...")
-#     paths, image_descriptors = read_image_descriptors(test_path, folders, max_items)
-#     print("Creating vocabulary...")
-#     vocab = create_vocabulary(image_descriptors)
-#     print("Dumping into pickle...")
-#     pickle.dump(vocab, open("./helper/vocab.p", "wb"))
-#     print("Computating vocabulary histograms...")
-#     vocab_hists = create_vocab_hists(paths, vocab)
-#     print("Dumping into pickle...")
-#     pickle.dump(vocab_hists, open("./helper/vocab_hists.p", "wb"))
-#     print("Finished sucessfully!")
-
-# #Procurar
-# def find(image, show=False):
-#     if not os.path.exists('./helper/'):
-#         print("ERROR: 'helper' folder does not exist!")
-#         print("Write -h or --help for more information.")
-#         exit(1)
-#     vocab = pickle.load(open("./helper/vocab.p", "rb"))
-#     vocab_hists = pickle.load(open("./helper/vocab_hists.p", "rb"))
-#     find_similar_bvw(image, vocab, vocab_hists, show)
-
-def main():
+def get_all_image_paths(dataset):
     paths = []
-    dataset = "dataset-projeto-3-2/"
     folders = os.listdir(dataset)
     for folder in folders:
         images = os.listdir(dataset + folder)
         temp = [dataset + folder + "/" + f for f in images]
         for p in temp:
             paths.append(p)
-    print(paths[0])
-    
+    return paths
 
-#     parser = argparse.ArgumentParser(description='Given a term, return the top 3 most similar images in the database.')
-#     parser.add_argument('keyword', type=str, help='Keyword for search')
-#     args = parser.parse_args()
-#     find(args.path, show=args.show)
+def mnetv2_loadimage(image_path):
+    img = image.load_img(image_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    return preprocess_input(x)
+
+def image_classification(paths):
+    classification_dict = {}
+    model = MobileNet(input_shape=None, alpha=1.0, depth_multiplier=1, include_top=True, weights='imagenet', input_tensor=None, pooling=None, classes=1000)
+    for path in paths:
+        image = mnetv2_loadimage(path)
+        preds = model.predict(image)
+        prediction = decode_predictions(preds, top=1)[0][0]
+        img_class = prediction[1]
+        img_confidence = prediction[2]
+        if img_class in classification_dict:
+            classification_dict[img_class].append((path, img_confidence))
+        else:
+            classification_dict[img_class] = [(path, img_confidence)]
+    return classification_dict
+
+#Treinar
+def train(dataset="dataset-projeto-3-2/"):
+    if not os.path.exists('./helper/'):
+        os.makedirs('./helper/')
+    print("Begin training...")
+    print("Getting all image paths...")
+    paths = get_all_image_paths(dataset)
+    print("Creating dict with paths...")
+    classification = image_classification(paths)
+    print(classification)
+    print("Dumping into pickle...")
+    pickle.dump(classification, open("./helper/dict.p", "wb"))
+    print("Finished sucessfully!")
+
+def greaterless(a, b):
+    if a > b:
+        return b
+    else:
+        return a
+
+def show_images(term, classifications, show=False):
+    print("Term used:", term)
+    match = classifications.get(term)
+    sorted_by_trust = sorted(match, reverse=True, key=lambda tup: tup[1])
+    size = greaterless(5, len(sorted_by_trust))
+    for i in range(size):
+        print("Path to Image: ", sorted_by_trust[i][0])
+        print("Score: ", sorted_by_trust[i][1])
+        if (show == True):
+            img = cv2.imread(sorted_by_trust[i][0], 1)
+            plt.imshow(img)
+            plt.title("Matched Image")
+            plt.show()
+        print("--------------")
+
+
+#Procurar
+def find(term, show=False):
+    if not os.path.exists('./helper/'):
+        print("ERROR: 'helper' folder does not exist!")
+        print("Write -h or --help for more information.")
+        exit(1)
+    paths_dict = pickle.load(open("./helper/dict.p", "rb"))
+
+    if term in paths_dict:
+        show_images(term, paths_dict, show)
+    else:
+        print("ERROR: used term '" + term + "' not valid.")
+        print("Please use these terms instead: https://gist.github.com/yrevar/942d3a0ac09ec9e5eb3a")
+        print("Write -h or --help for more information.")
+        exit(1)
+
+def main():
+    parser = argparse.ArgumentParser(description='Given a term, return the top 5 most similar images in the database.')
+    parser.add_argument('keyword', type=str, help='Keyword for search')
+    parser.add_argument('-s', '--show', action='store_true', help='Shows the 5 images instead of returning the path')
+    parser.add_argument('-t', '--train', action='store_true', help='Returns pickle objects in "helper" folder')
+    args = parser.parse_args()
+    if (args.train == True):
+        train(args.keyword)
+    else:
+        find(args.keyword, show=args.show)
 
 if __name__ == '__main__':
     main()
