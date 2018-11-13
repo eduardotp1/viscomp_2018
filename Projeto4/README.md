fcc80def27f68389899de4f032b54272827343a3
Filipe Borba <filipefborba@gmail.com>
2018-11-13 10:43:51 -0200

    argparse & readme

---
+++ Projeto4/README.md
@@ -0,0 +1,23 @@
+# Realidade Aumentada
+Filipe Borba, Gabriel Moreira, Guilherme Graicer
+6º Semestre<br>
+Engenharia da Computação Insper<br>
+Visão Computacional<br>
+
+## Sumário
+
+- [Como executar](#comoexecutar)
+- [Conceitos](#conceitos)
+
+## Como executar
+
+Para executar o programa, primeiro, verifique o caminho da imagem a ser substituída, assim como o caminho da imagem aruco_board.png, que deve estar na mesma pasta do programa python.
+Com o caminho da imagem de substituição em mãos, basta rodar o seguinte comando:
+
+`python3 detector.py "caminho_ate_imagem"`
+
+- O caminho_ate_imagem é o argumento OBRIGATORIO para o caminho no qual se encontra a imagem.
+
+## Conceitos
+
+
--- Projeto4/detector.py
+++ Projeto4/detector.py
@@ -1,61 +1,72 @@
 import cv2
 import cv2.aruco as aruco
 import numpy as np
+import argparse
 
-#Load reference image and replacement image
-replacement_img = cv2.imread("img.jpg")
-board_img = cv2.imread("board_aruco.png")
+def augment_reality(path):
+    #Load reference image and replacement image
+    replacement_img = cv2.imread(path)
+    board_img = cv2.imread("board_aruco.png")
 
-#Detect markers in reference image and get origin points
-board_img_gray = cv2.cvtColor(board_img, cv2.COLOR_BGR2GRAY)
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-parameters =  aruco.DetectorParameters_create()
-board_markers, board_ids, _ = aruco.detectMarkers(board_img_gray, aruco_dict, parameters=parameters)
-orig_points = np.asarray(board_markers).reshape((-1,2))
-
-# Using webcam
-cap = cv2.VideoCapture(0)
-while(True):
-    ret, frame = cap.read() #640x480
-    frame_copy = frame.copy()
-
-    # Detecting markers in real time
-    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #Detect markers in reference image and get origin points
+    board_img_gray = cv2.cvtColor(board_img, cv2.COLOR_BGR2GRAY)
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
     parameters =  aruco.DetectorParameters_create()
-    markers, ids, _ = aruco.detectMarkers(gray_image, aruco_dict, parameters=parameters)
+    board_markers, board_ids, _ = aruco.detectMarkers(board_img_gray, aruco_dict, parameters=parameters)
+    orig_points = np.asarray(board_markers).reshape((-1,2))
 
-    if (len(markers) == 0):
-        print("Nenhum marcador encontrado.")
-    else:
-        dest_points = np.asarray(markers).reshape((-1,2))
+    # Using webcam
+    cap = cv2.VideoCapture(0)
+    while(True):
+        ret, frame = cap.read() #640x480
+        frame_copy = frame.copy()
 
-        # Find the reference coordinate of the detected markers
-        ids = np.asarray(ids)
-        detected_orig_points = []
-        for id in ids:
-            index = np.argwhere(board_ids == id)[0][0]
-            for i in range(4):
-                detected_orig_points.append(orig_points[(index*4)+i])
-        detected_orig_points = np.asarray(detected_orig_points).astype(int)
+        # Detecting markers in real time
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+        parameters =  aruco.DetectorParameters_create()
+        markers, ids, _ = aruco.detectMarkers(gray_image, aruco_dict, parameters=parameters)
 
-        # Black image with replacement image over markers
-        homography = cv2.findHomography(detected_orig_points, dest_points)[0]
-        h, w, _ = frame.shape
-        warped_image = cv2.warpPerspective(replacement_img, homography, (w, h), frame_copy)
+        if (len(markers) == 0):
+            print("Nenhum marcador encontrado.")
+        else:
+            dest_points = np.asarray(markers).reshape((-1,2))
 
-        # Optional -- draw detected markers in image
-        # aruco.drawDetectedMarkers(frame, markers)
+            # Find the reference coordinate of the detected markers
+            ids = np.asarray(ids)
+            detected_orig_points = []
+            for id in ids:
+                index = np.argwhere(board_ids == id)[0][0]
+                for i in range(4):
+                    detected_orig_points.append(orig_points[(index*4)+i])
+            detected_orig_points = np.asarray(detected_orig_points).astype(int)
 
-        # Overlapping images
-        mask = warped_image > [0, 0, 0]
-        frame = np.where(mask == True, warped_image, frame)
+            # Black image with replacement image over markers
+            homography = cv2.findHomography(detected_orig_points, dest_points)[0]
+            h, w, _ = frame.shape
+            warped_image = cv2.warpPerspective(replacement_img, homography, (w, h), frame_copy)
 
-    # Show image, press q to exit
-    cv2.imshow('clean image', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            # Optional -- draw detected markers in image
+            # aruco.drawDetectedMarkers(frame, markers)
 
-# Clean everything up
-cap.release()
-cv2.destroyAllWindows()
+            # Overlapping images
+            mask = warped_image > [0, 0, 0]
+            frame = np.where(mask == True, warped_image, frame)
+
+        # Show image, press q to exit
+        cv2.imshow('clean image', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Clean everything up
+    cap.release()
+    cv2.destroyAllWindows()
+
+def main():
+    parser = argparse.ArgumentParser(description='Replace aruco markers image with another image of your choice.')
+    parser.add_argument('path', type=str, help='Path to replacement image')
+    args = parser.parse_args()
+    augment_reality(args.path)
+
+if __name__ == '__main__':
+    main()
